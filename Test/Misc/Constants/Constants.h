4bbb133b554bc8b27391038e2e4fa5b2853aca6c
Ilia Isupov <swinx89@gmail.com>
2015-04-14 18:29:31 +0300

    Reworked project folders structure

---
+++ Test/Misc/Constants/Constants.h
@@ -0,0 +1,12 @@
+//
+//  Constants.h
+//  Test
+//
+//  Created by Ilia Isupov on 14.04.15.
+//  Copyright (c) 2015 AnyWayAnyDay. All rights reserved.
+//
+
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#endif
---
+++ Test/Misc/Constants/Constants.m
@@ -0,0 +1,10 @@
+//
+//  Constants.m
+//  Test
+//
+//  Created by Ilia Isupov on 14.04.15.
+//  Copyright (c) 2015 AnyWayAnyDay. All rights reserved.
+//
+
+#import "Constants.h"
+
