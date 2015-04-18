2090a0954627fa89915159e0bc6a1b480ae6cda4
Ilia Isupov <swinx89@gmail.com>
2015-04-18 11:03:48 +0300

    Added AirlinesList controller

---
+++ Test/Controllers/AirlinesListController/AirlinesListController.h
@@ -0,0 +1,13 @@
+//
+//  AirlinesListController.h
+//  Test
+//
+//  Created by Ilia Isupov on 18.04.15.
+//  Copyright (c) 2015 AnyWayAnyDay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AirlinesListController : UIViewController
+
+@end
---
+++ Test/Controllers/AirlinesListController/AirlinesListController.m
@@ -0,0 +1,39 @@
+//
+//  AirlinesListController.m
+//  Test
+//
+//  Created by Ilia Isupov on 18.04.15.
+//  Copyright (c) 2015 AnyWayAnyDay. All rights reserved.
+//
+
+#import "AirlinesListController.h"
+
+@interface AirlinesListController ()<UITableViewDataSource, UITableViewDelegate>
+
+@property (nonatomic, weak) IBOutlet UITableView* table;
+
+@end
+
+@implementation AirlinesListController
+
+#pragma mark - UIViewController lifecycle
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+#pragma mark - UITableViewDataSource implementation
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 0;
+}
+
+-(UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return nil;
+}
+
+#pragma mark - UITableViewDelegate implementation
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+@end
